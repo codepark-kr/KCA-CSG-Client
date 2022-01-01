dfc4c8c7d5575503324d1569feb8f84870b688aa
codepakr-kr <codepark.kr@gmail.com>
2022-01-02 03:21:54 +0900

    [CFG] Initialize Spring Boot with Gradle project

---
+++ src/main/java/com/shelter/kcacsgclient/KcaCsgClientApplication.java
@@ -0,0 +1,13 @@
+package com.shelter.kcacsgclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcaCsgClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KcaCsgClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shelter/kcacsgclient/KcaCsgClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shelter.kcacsgclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcaCsgClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
