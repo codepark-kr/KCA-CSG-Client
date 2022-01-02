2f25e57d796afc2932069a93c1bf0bd47b9b1cee
codepakr-kr <codepark.kr@gmail.com>
2022-01-02 21:55:45 +0900

    [ADD] Add useState and onClick event for test api

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import { React, useState } from 'react';
+import axios from "axios";
 
 const Home=()=>{
+    const [keyword, setKeyword] = useState("No, It's not working.");
+
+    const getKeyword=async()=>{
+        axios.get('http://localhost:8080/api/test')
+            .then((response)=>{ setKeyword(response.data.message) })
+            .catch() }
+
     return(
         <>
-        <h2>Home</h2>
+        <h2>Home : { keyword } </h2>
+            <input type="button" value="call api" onClick={ getKeyword } />
         </>
     );
 }
